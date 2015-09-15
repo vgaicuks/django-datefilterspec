860152159ebcd5d4f732b42a11cfbd2034c4b9d8
Eduardo Matos <eduardo.matos@sieve.com.br>
2015-09-15 17:35:47 -0300

    remove useless django test configuration

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,23 +2,7 @@ from unittest import TestCase
 from django.conf import settings
 
 
-settings.configure(
-    DEBUG=True,
-    USE_TZ=True,
-    DATABASES={
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': ':memory:'
-        }
-    },
-    INSTALLED_APPS=[
-        'django.contrib.auth',
-        'django.contrib.contenttypes',
-        'django.contrib.sites',
-        'daterange_filter',
-    ],
-    SITE_ID=1,
-)
+settings.configure(USE_TZ=True)
 
 try:
     import django
