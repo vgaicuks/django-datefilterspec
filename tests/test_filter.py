d7b48c80ca94b16bb080fcb032c8323654338511
Eduardo Matos <eduardo.matos@sieve.com.br>
2015-09-15 13:19:30 -0300

    add form tests

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -1,7 +1,10 @@
+from mock import Mock
 from unittest import TestCase
 from django import forms
-from daterange_filter.filter import DateRangeFilterBaseForm
-from mock import Mock
+from django.contrib.admin.widgets import AdminDateWidget
+from django.utils.translation import ugettext as _
+from daterange_filter.filter import DateRangeFilterBaseForm, DateRangeForm, DateTimeRangeForm, \
+    DateRangeFilterAdminSplitDateTime
 
 
 class DateRangeFilterBaseFormTest(TestCase):
@@ -29,3 +32,61 @@ class DateRangeFilterBaseFormTest(TestCase):
 
         self.assertNotEqual(str(form_1.media), '')
         self.assertEquals(str(form_2.media), '')
+
+
+class DateRangeFormTest(TestCase):
+
+    class DummyForm(DateRangeForm):
+        pass
+
+    def test_create_fields(self):
+        form = self.DummyForm(Mock(), field_name='spam')
+
+        self.assertIsInstance(form.fields['spam__gte'], forms.DateField)
+        self.assertIsInstance(form.fields['spam__lte'], forms.DateField)
+
+    def test_field_attributes(self):
+        form = self.DummyForm(Mock(), field_name='ham')
+
+        self.assertEquals(form.fields['ham__gte'].label, '')
+        self.assertEquals(form.fields['ham__gte'].localize, True)
+        self.assertEquals(form.fields['ham__gte'].required, False)
+        self.assertIsInstance(form.fields['ham__gte'].widget, AdminDateWidget)
+        self.assertDictContainsSubset({'placeholder': _('From date')},
+                                      form.fields['ham__gte'].widget.attrs)
+
+        self.assertEquals(form.fields['ham__lte'].label, '')
+        self.assertEquals(form.fields['ham__lte'].localize, True)
+        self.assertEquals(form.fields['ham__lte'].required, False)
+        self.assertIsInstance(form.fields['ham__lte'].widget, AdminDateWidget)
+        self.assertDictContainsSubset({'placeholder': _('To date')},
+                                      form.fields['ham__lte'].widget.attrs)
+
+
+class DateTimeRangeFormTest(TestCase):
+
+    class DummyForm(DateTimeRangeForm):
+        pass
+
+    def test_create_fields(self):
+        form = self.DummyForm(Mock(), field_name='spam')
+
+        self.assertIsInstance(form.fields['spam__gte'], forms.DateTimeField)
+        self.assertIsInstance(form.fields['spam__lte'], forms.DateTimeField)
+
+    def test_field_attributes(self):
+        form = self.DummyForm(Mock(), field_name='ham')
+
+        self.assertEquals(form.fields['ham__gte'].label, '')
+        self.assertEquals(form.fields['ham__gte'].localize, True)
+        self.assertEquals(form.fields['ham__gte'].required, False)
+        self.assertIsInstance(form.fields['ham__gte'].widget, DateRangeFilterAdminSplitDateTime)
+        self.assertDictContainsSubset({'placeholder': _('From date')},
+                                      form.fields['ham__gte'].widget.attrs)
+
+        self.assertEquals(form.fields['ham__lte'].label, '')
+        self.assertEquals(form.fields['ham__lte'].localize, True)
+        self.assertEquals(form.fields['ham__lte'].required, False)
+        self.assertIsInstance(form.fields['ham__lte'].widget, DateRangeFilterAdminSplitDateTime)
+        self.assertDictContainsSubset({'placeholder': _('To date')},
+                                      form.fields['ham__lte'].widget.attrs)
